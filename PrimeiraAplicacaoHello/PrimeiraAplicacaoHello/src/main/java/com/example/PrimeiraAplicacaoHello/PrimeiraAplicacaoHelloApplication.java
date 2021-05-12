5da0209f42f0a1f7e5c078fcfa08877bfa9d339e
unknown <--contatobabiy@gmai.com>
2021-05-11 23:59:39 -0300

    atualizado

---
+++ PrimeiraAplicacaoHello/PrimeiraAplicacaoHello/src/main/java/com/example/PrimeiraAplicacaoHello/PrimeiraAplicacaoHelloApplication.java
@@ -0,0 +1,13 @@
+package com.example.PrimeiraAplicacaoHello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiraAplicacaoHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiraAplicacaoHelloApplication.class, args);
+	}
+
+}
---
+++ PrimeiraAplicacaoHello/PrimeiraAplicacaoHello/src/test/java/com/example/PrimeiraAplicacaoHello/PrimeiraAplicacaoHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PrimeiraAplicacaoHello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiraAplicacaoHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
