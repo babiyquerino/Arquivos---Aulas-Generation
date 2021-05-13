a532390179404af2d951bf8b15d06b3d97bba720
unknown <--contatobabiy@gmai.com>
2021-05-13 12:25:22 -0300

    atualizado

---
+++ LojaGame-main/src/test/java/com/lojagamer/lojagamer/LojagamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojagamer.lojagamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojagamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
